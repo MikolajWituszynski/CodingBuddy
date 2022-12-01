c77e656b99b87afc2d804abd5af8c1d8356dd4ba
Mikołaj <mikolaj.wituszynski@gmail.com>
2022-12-01 21:56:47 +0100

    Creation of HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./components/HomePage/HomePage";
-import LoginForm from "./components/LoginForm/LoginPage";
+import LoginPage from "./components/LoginPage/LoginPage";
 function App() {
   return (
     <div className="App">
